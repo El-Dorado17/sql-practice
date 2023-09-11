dc7630a11643e85a57378fd630946d5a44a0c182
Ely Dorado <2018dorado@gmail.com>
2023-09-11 14:16:46 -0500

    ORDER BY queries

--- test.sql
+++ test.sql
@@ -12,3 +12,22 @@ SELECT * FROM videogames
 --  Rank, Name, Platform, Year, Genre, Publisher,
 --  NA_Sales, EU_Sales, JP_Sales, Other_Sales, 
 --  Global_Sales
+
+
+SELECT NAME, PLATFORM, YEAR, NA_Sales
+FROM videogames
+ORDER BY NA_Sales DESC
+--*RETURNS
+--List of games by North American Sales 
+-- in descending order 
+-- Wii Sports (2006), Super Mario Bros (1985), and Duck Hunt (1984) are top 3
+
+
+SELECT NAME, PLATFORM, YEAR, NA_Sales
+FROM videogames
+WHERE Platform = 'PSP'
+ORDER BY NA_Sales DESC
+--*RETRUNS
+-- List of top selling PSP Games in NA
+-- GTA: Liberty City Stories  with 2.9 million USD in sales
+
