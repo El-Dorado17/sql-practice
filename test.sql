bd4384b7e72a03f060b5a7f3c36065451717d858
Ely Dorado <2018dorado@gmail.com>
2023-09-11 14:09:49 -0500

    Get entire table

---
+++ test.sql
@@ -0,0 +1,14 @@
+
+/*
+Database is a GIANT CSV File of 
+top 1,700 selling video games before February 2020
+
+Below are SQL queries used to get specific data
+*/
+
+
+SELECT * FROM videogames
+--*RETURNS
+--  Rank, Name, Platform, Year, Genre, Publisher,
+--  NA_Sales, EU_Sales, JP_Sales, Other_Sales, 
+--  Global_Sales
