c9bb1d84fda32e137cd55c5a924d68c89359dc2c
Ely Dorado <2018dorado@gmail.com>
2023-10-27 23:58:09 -0500

    prep

--- notes.sql
+++ notes.sql
@@ -6,3 +6,23 @@ SQL Wildcard and Special operators [22 Exercises]
 SQL Aggregate Functions [25 Exercises]
 SQL Formatting query output [10 Exercises]
 SQL Quering on Multiple Tables [8 Exercises]
+
+
+SQL Practice Websites
+SQLZoo
+SQL Fiddle
+DB-Fiddle
+DB<>Fiddle
+HackerRank
+SQL Bolt
+Oracle Live SQL
+W3Schools
+W3Resource
+StackOverflow Data Explorer
+Coding Ground by Tutorials Point
+SQL Course
+Tech on the Net
+SQL-ex
+Rextester
+SQLize
+SQL Test
