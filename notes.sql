7db26db062a0dd648584158e510fdcc8ab7630e5
Ely Dorado <2018dorado@gmail.com>
2023-10-27 23:56:15 -0500

    prep

--- notes.sql
+++ notes.sql
@@ -1,2 +1,8 @@
 For Monday: MAJOR PRACTICE FOR INTERVIEW
 
+SQL Retrieve data from tables [33 Exercises]
+SQL Boolean and Relational operators [12 Exercises]
+SQL Wildcard and Special operators [22 Exercises]
+SQL Aggregate Functions [25 Exercises]
+SQL Formatting query output [10 Exercises]
+SQL Quering on Multiple Tables [8 Exercises]
