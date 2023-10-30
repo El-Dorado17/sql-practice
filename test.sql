e2fcea8117bfffb4fa597c1b151937f7898c360c
Ely Dorado <2018dorado@gmail.com>
2023-10-30 16:08:20 -0500

    Examples

--- test.sql
+++ test.sql
@@ -236,21 +236,34 @@ ORDER BY Rank asc
 DEFINITIONS:
 
 SELECT
-    Indicate what we will are Querying about
+    -- Indicate what we will are Querying about
     SELECT population FROM world
 
 
 FROM
-    Indicate what table we are talking about
-    SELECT ___ FROM video-game-sales
+    -- Indicate what table we are talking about
+    SELECT * FROM sales
 
 WHERE
+    -- Specify criteria
+    SELECT * FROM sales
+    WHERE North_American_Sales > 100,000
 
 INSERT INTO
+    -- Adds new data into a table
+    INSERT INTO Employees (EmployeeID, FirstName, LastName)
+    VALUES (444, Ely, Dorado)
 
 UPDATE
+    --Basically a PUT request
+    Update Employees
+    SET Status = 'Hired'
+    WHERE EmployeeID = 444
 
 DELETE
+    -- Deletes a line of data
+    DELETE FROM Products
+    WHERE ProductID - 72
 
 CREATE TABLE
 
