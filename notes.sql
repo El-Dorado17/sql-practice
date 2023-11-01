653de2edf22a9265aee44fed7ccb0a78e152e705
Ely Dorado <2018dorado@gmail.com>
2023-11-01 14:47:30 -0500

    sql practice

--- notes.sql
+++ notes.sql
@@ -70,18 +70,18 @@ CASE
 UNION
 
 
-JOINS - LEFT & INNER
-    SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
-    FROM Orders
-    INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+-- JOINS - LEFT & INNER
+--     SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+--     FROM Orders
+--     INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 
-    (INNER) JOIN: Returns records that have matching values in both tables
-    LEFT (OUTER) JOIN: Returns all records from the left table, and the matched records from the right table
-    RIGHT (OUTER) JOIN: Returns all records from the right table, and the matched records from the left table
-    FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table
+--     (INNER) JOIN: Returns records that have matching values in both tables
+--     LEFT (OUTER) JOIN: Returns all records from the left table, and the matched records from the right table
+--     RIGHT (OUTER) JOIN: Returns all records from the right table, and the matched records from the left table
+--     FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table
 
-Groupby
-count vs sum
-case when/IF
-Rank() Row_Number()
-Where vs HAVING
+-- Groupby
+-- count vs sum
+-- case when/IF
+-- Rank() Row_Number()
+-- Where vs HAVING  
